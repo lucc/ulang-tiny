9e384b5945103a87765bd82fa1b1f8fef1921dd4
Lucas Hoffmann <lucas.hoffmann@campus.lmu.de>
2021-02-06 07:10:08 +0100

    Add implicit ulang parser to tests

--- ulang/test/src/ProofCheckTest.scala
+++ ulang/test/src/ProofCheckTest.scala
@@ -7,13 +7,16 @@ class ProofCheckTest extends AnyFunSpec {
   // load the prelude file when initializeing the test suite
   ulang.Main.loadPrelude()
 
-  def parse(s: String) = {
-    import arse._
-    implicit val w = ulang.Parse.whitespace
-    ulang.Parse.expr.parseAll(s)
-  }
   def assertProves(proof: Expr, goal: Expr) {
     assert(check(Map(), proof, goal), proof + " does not prove " + goal)
+  }
+  /** A String interpolator to parse u"..." into Expr.  */
+  implicit class UlangParser(val sc: StringContext) {
+    def u(): Expr = {
+      import arse._
+      implicit val w = ulang.Parse.whitespace
+      ulang.Parse.expr.parseAll(sc.parts.mkString(" "))
+    }
   }
 
   describe("simple proofs") {
@@ -21,42 +24,39 @@ class ProofCheckTest extends AnyFunSpec {
       assertProves(True, True)
     }
     it("the identity function proofs a==>a") {
-      val id_term = parse("lambda x -> x")
-      assertProves(id_term, Imp(Id("a"), Id("a")))
+      val id_term = u"lambda x -> x"
+      assertProves(id_term, u"a ==> a")
     }
   }
 
   describe("Hilbert axioms") {
     it("cons proves weakening") {
-      val cons = parse("lambda x -> lambda y -> x")
-      val weakening = Imp(Id("a"), Imp(Id("b"), Id("a")))
+      val cons = u"lambda x -> lambda y -> x"
+      val weakening = u"a ==> (b ==> a)"
       assertProves(cons, weakening)
     }
     it("flip function proof order innvariance in implications") {
-      val flip = parse("lambda x -> lambda y -> lambda z -> x z y")
-      val invariance = Imp(Imp(Id("a"), Imp(Id("b"), Id("c"))),
-                           Imp(Id("b"), Imp(Id("a"), Id("c"))))
+      val flip = u"lambda x -> lambda y -> lambda z -> x z y"
+      val invariance = u"(a ==> b ==> c) ==> b ==> a ==> c"
       assertProves(flip, invariance)
     }
     it("function composition proves chaining") {
-      val composition = parse("lambda f -> lambda g -> lambda x -> f (g x)")
-      val chain = Imp(Imp(Id("b"), Id("c")),
-                      Imp(Imp(Id("a"), Id("b")),
-                          Imp(Id("a"), Id("c"))))
+      val composition = u"lambda f -> lambda g -> lambda x -> f (g x)"
+      val chain = u"(b ==> c) ==> (a ==> b) ==> a ==> c"
       assertProves(composition, chain)
     }
     it("ex falso quodlibet") {
       val foo = ???
-      val efq = Imp(Id("a"), Imp(Imp(Id("a"), False), Id("b")))
+      val efq = u"a ==> (not a ==> b)"
       assertProves(foo, efq)
     }
   }
 
   describe("conjunction") {
     it("is symmetric") {
-      val switch1 = parse("lambda (x,y) -> (y,x)")
-      val switch2 = parse("lambda p -> match p with (x,y) -> (y,x)")
-      val sym = Imp(And(Id("a"), Id("b")), And(Id("b"), Id("a")))
+      val switch1 = u"lambda (x,y) -> (y,x)"
+      val switch2 = u"lambda p -> match p with (x,y) -> (y,x)"
+      val sym = u"a /\ b ==> b /\ a"
       assertProves(switch1, sym)
       assertProves(switch2, sym)
     }
