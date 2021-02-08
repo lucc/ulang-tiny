6de8a5ec6a2af2280eb7efcaa637545ef701906f
Lucas Hoffmann <lucas.hoffmann@campus.lmu.de>
2021-02-08 14:15:49 +0100

    Upgrade some ulang examples to tests

--- ulang/test/src/RunUlangTests.scala
+++ ulang/test/src/RunUlangTests.scala
@@ -3,12 +3,20 @@ import java.io.File
 
 class RunUlangTests extends AnyFunSpec with PreloadLoader {
 
-  val testFiles = new File(getClass.getResource("/").getFile()).listFiles(
+  val testFiles = new File(getClass.getResource("/tests").getFile()).listFiles(
     (_, name) => name endsWith ".u").map(_.getAbsolutePath)
 
   describe("run file") {
     for (testFile <- testFiles)
       it(testFile) { ulang.Exec.run(testFile) }
+  }
+
+  describe("special files") {
+    it("the omega term should throw an exception") {
+      assertThrows[java.lang.StackOverflowError] {
+        ulang.Exec.run(getClass.getResource("/omega.u").getPath())
+      }
+    }
   }
 
 }
