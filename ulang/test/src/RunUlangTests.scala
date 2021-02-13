30841b8782275fae296e1d76acf810b3a2483e74
Lucas Hoffmann <lucas.hoffmann@campus.lmu.de>
2021-02-13 14:59:09 +0100

    Rename pending test files for better highlighting in vim

--- ulang/test/src/RunUlangTests.scala
+++ ulang/test/src/RunUlangTests.scala
@@ -3,14 +3,14 @@ import java.io.File
 
 class RunUlangTests extends AnyFunSpec with PreloadLoader {
 
-  def testFiles(extension: String) =
-    new File(getClass.getResource("/tests").getFile()).listFiles(
-      (_, name) => name endsWith extension).map(_.getAbsolutePath)
+  def testFiles(folder: String) =
+    new File(getClass.getResource("/"+folder).getFile()).listFiles(
+      (_, name) => name endsWith ".u").map(_.getAbsolutePath)
 
   describe("run file") {
-    for (testFile <- testFiles(".u"))
+    for (testFile <- testFiles("tests"))
       it(testFile) { ulang.Exec.run(testFile) }
-    for (testFile <- testFiles(".u.pending"))
+    for (testFile <- testFiles("pending"))
       it(testFile) {
         pendingUntilFixed {
           ulang.Exec.run(testFile)
