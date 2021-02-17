6c5851d079e03194fe79463863a508beca204334
Lucas Hoffmann <lucas.hoffmann@campus.lmu.de>
2021-02-17 09:44:12 +0100

    Run failing proofs in individual tests

---
+++ ulang/test/src/ProofSnippetsTest.scala
@@ -0,0 +1,36 @@
+import org.scalatest.funspec.AnyFunSpec
+
+class ProofSnippetsTest extends AnyFunSpec with PreloadLoader {
+  val pendingProofs = List(
+    // order infariance
+    """
+    show (a ==> b ==> c) ==> b ==> a ==> c;
+    proof term lambda x -> lambda y -> lambda z -> x z y;
+    """,
+    // chaining
+    """
+    show (b ==> c) ==> (a ==> b) ==> a ==> c;
+    proof term lambda f -> lambda g -> lambda x -> f (g x);
+    """,
+    // modus ponens
+    """
+    show a ==> (a ==> b) ==> b;
+    proof term lambda x -> lambda f -> f x;
+    """,
+    // or elimination
+    """
+    show a \/ b ==> (a ==> c) ==> (b ==> c) ==> c;
+    proof lambda (Left x)  -> (lambda p1 -> lambda p2 -> p1 x)
+               | (Right x) -> (lambda p1 -> lambda p2 -> p2 x);
+    """
+  )
+  val pendingDefines = List(
+    )
+  val pendingScripts = pendingProofs ++ pendingDefines
+  describe("test small pending scripts") {
+    for (snippet <- pendingScripts) {
+      it(snippet) { pendingUntilFixed { ulang.Exec.run(snippet) } }
+    }
+
+  }
+}
