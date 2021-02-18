51eb46b763cc1952f22a21af283228c2f95bd849
Lucas Hoffmann <lucas.hoffmann@campus.lmu.de>
2021-02-18 10:59:42 +0100

    Add test for discussion

--- ulang/test/src/ProofSnippetsTest.scala
+++ ulang/test/src/ProofSnippetsTest.scala
@@ -22,7 +22,8 @@ class ProofSnippetsTest extends AnyFunSpec with PreloadLoader {
     show a \/ b ==> (a ==> c) ==> (b ==> c) ==> c;
     proof lambda (Left x)  -> (lambda p1 -> lambda p2 -> p1 x)
                | (Right x) -> (lambda p1 -> lambda p2 -> p2 x);
-    """
+    """,
+    "show a ==> a; proof term (lambda v -> (lambda u -> u) v);",
   )
   val pendingDefines = List(
     )
