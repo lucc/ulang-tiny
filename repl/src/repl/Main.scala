34e4efabfdad3febe5b23f24aab0349af2f75d8e
Lucas Hoffmann <lucas.hoffmann@campus.lmu.de>
2021-07-04 16:19:03 +0200

    repl: colour prompt and errors

--- repl/src/repl/Main.scala
+++ repl/src/repl/Main.scala
@@ -2,24 +2,26 @@
 import ulang.Main.loadPrelude
 import ulang.Exec._
 import scala.io.StdIn.readLine
+import Console.{RED, BLUE, RESET}
 
 object Repl {
 
-  val prompt = "ulang> "
+  def prompt = print(BLUE + "ulang> " + RESET)
+  def error(message: String) = println(RED + message + RESET)
 
   def main(args: Array[String]) = {
     println("Starting the Ulang REPL ...")
     loadPrelude()
 
-    print(prompt)
+    prompt
     for (line <- io.Source.stdin.getLines) {
       try {
         exec(parse(line))
       } catch {
-        case e: arse.Error =>
-          println("Parse error: " + e)
+        case e: arse.Error => error("Parse error: " + e)
+        case e: RuntimeException => error("Error: " + e.getMessage())
       }
-      print(prompt)
+      prompt
     }
   }
 }
